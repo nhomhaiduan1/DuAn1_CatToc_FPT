50d6c2af70ce3145abf6cf3cf95c4e03ea10babe
nhomhaiduan1 <nhomhaiduan1@gmail.com>
2023-11-16 17:51:03 +0700

    Giao Dien Man Hinh

---
+++ app/src/main/java/com/example/duan_cattoc/manhinhcho.java
@@ -0,0 +1,28 @@
+package com.example.duan_cattoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class manhinhcho extends AppCompatActivity {
+TextView appname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhcho);
+appname=findViewById(R.id.appname);
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(manhinhcho.this, DangNhap.class);
+                startActivity(intent);
+            }
+        }, 3500);
+    }
+}
