f238c1e4a6d1a210453a08f7e1d87f2c6f2d1566
nhomhaiduan1 <nhomhaiduan1@gmail.com>
2023-11-23 17:55:02 +0700

    giaodienthemnhanvien

--- app/src/main/java/com/example/duan_cattoc/fragment_ThemNhanVien.java
+++ app/src/main/java/com/example/duan_cattoc/fragment_ThemNhanVien.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 public class fragment_ThemNhanVien extends Fragment {
+    public fragment_ThemNhanVien() {
+        // Required empty public constructor
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.activity_fragment_them_nhan_vien,container,false);
 
+        return v;
+    }
 }
