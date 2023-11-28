1b4bbc1ddf17a7ed769019b4ced95ac76d8a0c81
HoangManhDA <manhthph13394@fpt.edu.vn>
2023-11-28 16:53:37 +0700

    lienhe + doanhthu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -53,4 +55,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/duan_cattoc/fragment_LienHe.java
+++ app/src/main/java/com/example/duan_cattoc/fragment_LienHe.java
@@ -1,8 +1,29 @@
 package com.example.duan_cattoc;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class fragment_LienHe extends Fragment {
+    TextView tvTT, tvEmail, tvZalo;
+    EditText EmailLH;
+
+    ImageView imageView;
 
 
+    @Override
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_fragment_lienhe, container, false);
+        return view;
+
+
+    }
 }
