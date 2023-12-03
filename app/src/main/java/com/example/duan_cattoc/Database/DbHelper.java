4f4b69b21d2fc54f44ca6dcadfc6b05c55a4cf57
nhomhaiduan1 <nhomhaiduan1@gmail.com>
2023-12-03 14:27:25 +0700

    fix lai

--- app/src/main/java/com/example/duan_cattoc/Database/DbHelper.java
+++ app/src/main/java/com/example/duan_cattoc/Database/DbHelper.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
 public static final String DB_NAME ="CATTOC_FPT";
-public static final int DB_VERSION = 18;
+public static final int DB_VERSION = 21;
 
 
     public DbHelper (@Nullable Context context) {
@@ -67,9 +67,7 @@ public static final int DB_VERSION = 18;
                 "(6,'Nguyễn Bảo Long','2005', '03283234444')," +
                 "(7,'Lê Thị Khánh Huyền','2006', '03283234888')");
         db.execSQL("INSERT INTO LoaiDichVu VALUES(1,'Cắt Tóc'),(2,'Nhuộm Tóc'),(3,'Uốn Tóc'),(4,'Gội Đầu'),(5,'Chăm Sóc Da')");
-        db.execSQL("INSERT INTO DichVu VALUES(1,'Cắt Layer',40,'2'),(2,'Cắt 3 phân',30,'1'),(3,'Cắt Sport',40,'3')," +
-                "(4,'Nhuộm Trà Sữa',200,'4'),(5,'Nhuộm Đỏ',150,'5'),(6,'Uốn Sóng',150,'6')," +
-                "(7,'Uốn Sâu',40,'7'),(8,'Gội Chuyên Sâu',100,'8')");
+        db.execSQL("INSERT INTO DichVu VALUES(1,'Cắt Layer',40,'1'),(2,'Cắt 3 phân',30,'2'),(3,'Cắt Sport',40,'3')");
         db.execSQL("INSERT INTO HoaDon VALUES(1,'admin','1','1','30','2023/12/2','1')," +
                 "(2,'NguyenVanM','2','2','20','2023/12/2','1')");
     }
